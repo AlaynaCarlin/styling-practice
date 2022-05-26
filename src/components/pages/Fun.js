0ffae8c3c416ea36aca89599f24e1d3160f18880
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-05-26 11:43:30 -0400

    using GIPHY api for dark page

--- src/components/pages/Fun.js
+++ src/components/pages/Fun.js
@@ -1,38 +1,18 @@
-import { Container, Button, Input, Form } from "reactstrap";
-import React, {useState, useEffect} from "react";
+import { Container, Button, Input, Form, Row } from "reactstrap";
+import React, { useState, useEffect } from "react";
 
-const apiKey = 'kP5tzfXV1T3W0J83DXmmC4WF1XbZ1V0KPpg5yIeJ';
+// const apiKey = 'kP5tzfXV1T3W0J83DXmmC4WF1XbZ1V0KPpg5yIeJ';
+
+//  cat facts ' https://alexwohlbruck.github.io/cat-facts/ ' 
 
 const Fun = () => {
 
-    const [asteroids, setAsteroids] = useState([]);
-    const [date, setDate] = useState();
+   
 
-    const fetchStuff = () => {
-        console.log('fetch stuff');
-
-        fetch (`https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-02-01&end_date=2021-02-01&detailed=true&api_key=${apiKey}`, {
-            method: 'GET'
-        }).then (res => res.json())
-        .then(json => {
-            console.log(json)
-            setAsteroids(json.near_earth_objects)
-        }).catch((err) => console.log(err))
-    }
-
-    return(
+    return (
         <div className="fun">
             <div className="mainDiv">
-                <Form>
-                    <Input 
-                        bsSize="lg"
-                        type="number"
-                        placeholder="23"
-                        onChange={(e) => setDate(e.target.value)}
-                        value={date}
-                    />
-                    <Button onClick={() => fetchStuff()}>Submit</Button>
-                </Form>
+               
             </div>
         </div>
     );
