7646953cfb19ad182d116fc11d92f1ea3f81318b
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-05-30 11:47:59 -0400

    dark page works, fetches and displays 25 gifs when searched

--- src/components/pages/Dark.js
+++ src/components/pages/Dark.js
@@ -1,31 +1,67 @@
-import { Button } from "reactstrap";
-import React, {useState} from "react";
+import { Button, Input, Form, Col, Row, List } from "reactstrap";
+import React, { useState, useEffect } from "react";
 
 // GIPHY API ' https://developers.giphy.com/ '
 
 // api key ' 84t03ALHsqYZ6PuIEn3T6Y1evJxmqo2l '
 
+// ? Add a reFetch
+
 const Dark = () => {
 
-    const [img1, setImg1] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchGifs, setSearchGifs] = useState([]);
 
     const fetchGif = () => {
-        fetch('https://api.giphy.com/v1/gifs/random?api_key=84t03ALHsqYZ6PuIEn3T6Y1evJxmqo2l&limit=5&rating=g', {
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=84t03ALHsqYZ6PuIEn3T6Y1evJxmqo2l&q=${searchTerm}&limit=25&offset=2&rating=g&lang=en`, {
             method: 'GET'
         }).then(res => res.json())
             .then(json => {
                 console.log(json)
-                setImg1(json.data.images.fixed_height.url);
-                console.log(img1);
+                setSearchGifs(json.data);
+                console.log(searchGifs);
             })
             .catch((error) => console.log(error))
     }
 
+    const gifMapper = () => {
+        console.log('gif map');
+
+        return searchGifs.map((gif, idx) => {
+            return (
+                <Col key={idx}>
+                    <div>
+                        <List type="unstyled">
+                            <img src={gif.images.fixed_height.url}/>
+                        </List>
+                    </div>
+                </Col>
+            )
+        })
+    }
+
+
     return (
         <div className="dark">
             <div className="mainDiv">
-                <Button onClick={ fetchGif }>Fetch</Button>
-                <img src={img1} alt='gif' width='20%' height='20%' />
+                <Form onSubmit={ e => { e.preventDefault(); fetchGif();}}>
+                    <Row xs='1' md='3' xl='3'>
+                        <Col>
+                            <h4>Search for a Gif</h4>
+                        </Col>
+                        <Col>
+                            <Input placeholder="search" onChange={(e) => setSearchTerm(e.target.value)} />
+                        </Col>
+                        <Col>
+                            <Button type="submit">Fetch</Button>
+                        </Col>
+                    </Row>
+                </Form>
+                <hr/>
+                {/* <img src={img1} alt='gif' width='20%' height='20%' /> */}
+                <Row xs="1" md="2" xl="3" style={{ overflow: "scroll", height: "100vh" }} >
+                    {gifMapper()}
+                </Row>
             </div>
         </div>
     );
