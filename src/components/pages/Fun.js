ef407b534f514ee8425359224e0f6d5ce4cc79bb
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-06-01 10:36:43 -0400

    three pages complete

--- src/components/pages/Fun.js
+++ src/components/pages/Fun.js
@@ -1,4 +1,4 @@
-import { Container, Button, Input, Form, Row } from "reactstrap";
+import { Container, Button, Input, Form, Row, Col } from "reactstrap";
 import React, { useState, useEffect } from "react";
 
 // const apiKey = 'kP5tzfXV1T3W0J83DXmmC4WF1XbZ1V0KPpg5yIeJ';
@@ -7,12 +7,40 @@ import React, { useState, useEffect } from "react";
 
 const Fun = () => {
 
-   
+    const [facts, setFacts] = useState([]);
+
+   const fetchCat = () => {
+       fetch(`https://cat-fact.herokuapp.com/facts`, {
+           method: 'GET'
+       }) .then (res => res.json())
+        .then(json => {
+            console.log(json)
+            setFacts(json);
+        })
+   }
+
+   const factMapper = () => {
+
+    return facts.map((fact, idx) => {
+        return (
+            <Row key={idx}>
+                <div>
+                    <p>{fact.text}</p>
+                </div>
+            </Row>
+        )
+    })
+   }
 
     return (
         <div className="fun">
             <div className="mainDiv">
-               
+               <Button onClick={fetchCat}>cat facts</Button>
+            </div>
+            <div>
+                <Col >
+                    {factMapper()}
+                </Col>
             </div>
         </div>
     );
